ea6cc2be15c0a2e920ad7b60a416d7e54728ff6c
Piyush-Pant2023 <piyushpant64@gmail.com>
2024-06-06 21:48:06 +0530

    basics

---
+++ java script/piyu.js
@@ -0,0 +1,54 @@
+let a=40;
+//const b=50;
+//a=50;
+//console.log("value of a is"a)//
+//function print(){
+  //  console.log(a)
+//}
+//print()
+//let obj={
+    "name":"pan",
+    "course":"btech"
+    console.log(obj.name,obj.course)
+//}
+let day =4
+switch(day){
+    case 1:console.log("monday");
+    break;
+    case 2:console.log("tuesday");
+    break;
+    case 3:console.log("wednesday");
+    break;
+    case 4:console.log("thrusaday");
+    break;
+    case 5:console.log("friday");
+    break;
+    case 6:console.log("saturday");
+    break;
+    case 7:console.log("sunday");
+    break;
+}
+//template literal
+
+name="pant"
+age=20
+console.log(`&{names}have age&{age}`)
+
+
+//for in loop
+let s1={"name":"piyush","college":"sit","class":"btech"}
+for (let i in s1){
+    console.log(i,s1[i])
+}
+//function
+function piyush(){
+    console.log("hello",a,b)
+
+}
+piyush(5,6)
+//arrow function
+let piyush=()=>{
+    console.log("hello",a,b)
+
+}
+piyush(5,6)
