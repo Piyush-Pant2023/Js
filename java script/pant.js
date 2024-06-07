f52251a19ab57b629de0507267f2654de93b0986
Piyush-Pant2023 <piyushpant64@gmail.com>
2024-06-07 20:19:40 +0530

    basics

---
+++ java script/pant.js
@@ -0,0 +1,15 @@
+const list[1,2,3,4,5]
+list[4]=7
+console.log(list[4])
+console.log()
+console.log(list)
+list2=[8,7,68,5]
+list3=[30.400.50]
+list.concat(list2)
+console.log(list.concat(list2,list4))
+let arr=[50,10,15,24]
+console.log(arr.sort().reverse())
+let arr=[1,2,3,4,5]
+arr.forEach((v,i))=>{
+console.log(v*4)
+};
