4e9a916c427709ec3cbd2a134d788b381279cebe
Piyush-Pant2023 <piyushpant64@gmail.com>
2024-07-16 22:43:00 +0530

    basic

---
+++ new/basics.js
@@ -0,0 +1,6 @@
+let d= new Date();
+console.log(d.getDate(),"/",d.getFullYear());
+console.log(d.getHours(),"/",d.getMinutes(),":",d.getSeconds());
+let date("10-27-2004")
+console.log(date.getDate(),date.getDay())
+
